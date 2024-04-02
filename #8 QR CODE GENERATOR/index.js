7244e8fc067e7627440e2cc9b5b0faa16b175bd1
Asilbek2002 <school2924@gmail.com>
2024-04-02 18:39:16 +0500

    QR GENERATOR APP

---
+++ #8 QR CODE GENERATOR/index.js
@@ -0,0 +1,16 @@
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+let btn = document.getElementById("btn");
+
+function generate() {
+  qrImage.src =
+    `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=` +
+    qrText.value;
+  // let imgBox = document.createElement('img')
+  imgBox.appendChild(qrImage);
+  imgBox.classList.add("show-img");
+  console.log("log");
+}
+
+// btn.addEventListener("click", generate());
